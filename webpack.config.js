51152e422c52f77bce877ef71fcaf19d4208725c
TobiWeiss <tobias.weiss@bytabo.de>
2022-01-07 22:15:35 +0100

    adjusted webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const env = dotenv.config().parsed;
+const env = process.env;
 
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
