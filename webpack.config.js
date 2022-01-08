8d4478ea43d5bd0cb0ba4a3f5425c0787f53d4ee
TobiWeiss <tobias.weiss@bytabo.de>
2022-01-08 14:56:54 +0100

    Revert "adjusted webpack"
    This reverts commit 51152e422c52f77bce877ef71fcaf19d4208725c.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const env = process.env;
+const env = dotenv.config().parsed;
 
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
