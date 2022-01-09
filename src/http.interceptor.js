f4f282a8b3946bccdab58904dd835c763f210215
TobiWeiss <tobias.weiss@bytabo.de>
2022-01-09 17:40:18 +0100

    added api-key to http-interceptor

--- src/http.interceptor.js
+++ src/http.interceptor.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.interceptors.request.use(
   (config) => {
     config.headers.Accept = 'application/json';
-
+    config.headers.Authorization = process.env.API_KEY;
     return config;
   },
   (error) => Promise.reject(error),
