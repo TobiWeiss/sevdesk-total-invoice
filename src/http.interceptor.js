dd9f6de3ab231fbf01586224988b535d4cec9f09
TobiWeiss <tobias.weiss@bytabo.de>
2022-01-09 18:12:07 +0100

    adjusted axios interceptor and excluded headers for method options

--- src/http.interceptor.js
+++ src/http.interceptor.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 axios.interceptors.request.use(request => {
-    request.config.headers.Accept = 'application/json';
-    if(request.method != "options") request.headers.Authorization = process.env.API_KEY;
+    console.info(request);
+    request.headers.common.Accept = 'application/json';
+    if(request.method != "options") request.headers.common.Authorization = process.env.API_KEY;
     return request;
 }, error => {
     console.log(error);
