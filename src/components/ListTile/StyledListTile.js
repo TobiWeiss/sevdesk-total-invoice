8c754247006279b79e6bc4094b6cc939d2d4111c
TobiWeiss <38571531+TobiWeiss@users.noreply.github.com>
2022-11-04 23:21:08 +0100

    Update StyledListTile.js
    removed animation from list tiles

--- src/components/ListTile/StyledListTile.js
+++ src/components/ListTile/StyledListTile.js
@@ -2,31 +2,6 @@ import styled from 'styled-components'
 import theme from '../../theme'
 
 const StyledListTile = styled.div`
-  opacity: 0;
-  animation: ${props =>
-    props.index % 2 === 0 ? 'fadeInLeft 2s' : 'fadeInRight 2s'};
-  animation-delay: ${props => props.index * 0.3}s;
-  animation-fill-mode: forwards;
-  @keyframes fadeInLeft {
-    0% {
-      opacity: 0;
-      transform: translateX(-2000px);
-    }
-    100% {
-      opacity: 1;
-      transform: translateX(0);
-    }
-  }
-  @keyframes fadeInRight {
-    0% {
-      opacity: 0;
-      transform: translateX(2000px);
-    }
-    100% {
-      opacity: 1;
-      transform: translateX(0);
-    }
-  }
   height: 63px;
   background: ${theme.colors.white};
   box-shadow: 0px 4px 29px rgba(0, 0, 0, 0.08);
